144b1e9c88e32f4fc2fb2487ed4f04a8fb9329d2
Francesco215 <f.sacco@protonmail.com>
2026-04-29 16:13:37 +0000

    works with hydra, still no nnx

--- scacchi/train.py
+++ scacchi/train.py
@@ -20,12 +20,13 @@ from functools import partial
 from typing import NamedTuple
 
 import haiku as hk
+import hydra
 import jax
 import jax.numpy as jnp
 import mctx
 import optax
 import pgx
-from omegaconf import OmegaConf
+from omegaconf import DictConfig, OmegaConf
 from pgx.experimental import auto_reset
 from pydantic import BaseModel
 
@@ -57,110 +58,12 @@ class Config(BaseModel):
         extra = "forbid"
 
 
-conf_dict = OmegaConf.from_cli()
-config: Config = Config(**conf_dict)
-print(config)
-
-env = pgx.make(config.env_id)
-baseline = pgx.make_baseline_model(config.env_id + "_v0")
-
-
-def forward_fn(x, is_eval=False):
-    net = AZNet(
-        num_actions=env.num_actions,
-        num_channels=config.num_channels,
-        num_blocks=config.num_layers,
-        resnet_v2=config.resnet_v2,
-    )
-    policy_out, value_out = net(x, is_training=not is_eval, test_local_stats=False)
-    return policy_out, value_out
-
-
-forward = hk.without_apply_rng(hk.transform_with_state(forward_fn))
-optimizer = optax.adam(learning_rate=config.learning_rate)
-
-
-def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
-    # model: params
-    # state: embedding
-    del rng_key
-    model_params, model_state = model
-
-    current_player = state.current_player
-    state = jax.vmap(env.step)(state, action)
-
-    (logits, value), _ = forward.apply(model_params, model_state, state.observation, is_eval=True)
-    # mask invalid actions
-    logits = logits - jnp.max(logits, axis=-1, keepdims=True)
-    logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
-
-    reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
-    value = jnp.where(state.terminated, 0.0, value)
-    discount = -1.0 * jnp.ones_like(value)
-    discount = jnp.where(state.terminated, 0.0, discount)
-
-    recurrent_fn_output = mctx.RecurrentFnOutput(
-        reward=reward,
-        discount=discount,
-        prior_logits=logits,
-        value=value,
-    )
-    return recurrent_fn_output, state
-
-
 class SelfplayOutput(NamedTuple):
     obs: jnp.ndarray
     reward: jnp.ndarray
     terminated: jnp.ndarray
     action_weights: jnp.ndarray
     discount: jnp.ndarray
-
-
-@jax.pmap
-def selfplay(model, rng_key: jnp.ndarray) -> SelfplayOutput:
-    model_params, model_state = model
-    batch_size = config.selfplay_batch_size // num_devices
-
-    def step_fn(state, key) -> SelfplayOutput:
-        key1, key2 = jax.random.split(key)
-        observation = state.observation
-
-        (logits, value), _ = forward.apply(
-            model_params, model_state, state.observation, is_eval=True
-        )
-        root = mctx.RootFnOutput(prior_logits=logits, value=value, embedding=state)
-
-        policy_output = mctx.gumbel_muzero_policy(
-            params=model,
-            rng_key=key1,
-            root=root,
-            recurrent_fn=recurrent_fn,
-            num_simulations=config.num_simulations,
-            invalid_actions=~state.legal_action_mask,
-            qtransform=mctx.qtransform_completed_by_mix_value,
-            gumbel_scale=1.0,
-        )
-        actor = state.current_player
-        keys = jax.random.split(key2, batch_size)
-        state = jax.vmap(auto_reset(env.step, env.init))(state, policy_output.action, keys)
-        discount = -1.0 * jnp.ones_like(value)
-        discount = jnp.where(state.terminated, 0.0, discount)
-        return state, SelfplayOutput(
-            obs=observation,
-            action_weights=policy_output.action_weights,
-            reward=state.rewards[jnp.arange(state.rewards.shape[0]), actor],
-            terminated=state.terminated,
-            discount=discount,
-        )
-
-    # Run selfplay for max_num_steps by batch
-    rng_key, sub_key = jax.random.split(rng_key)
-    keys = jax.random.split(sub_key, batch_size)
-    state = jax.vmap(env.init)(keys)
-    key_seq = jax.random.split(rng_key, config.max_num_steps)
-    _, data = jax.lax.scan(step_fn, state, key_seq)
-
-    return data
 
 
 class Sample(NamedTuple):
@@ -170,94 +73,182 @@ class Sample(NamedTuple):
     mask: jnp.ndarray
 
 
-@jax.pmap
-def compute_loss_input(data: SelfplayOutput) -> Sample:
-    batch_size = config.selfplay_batch_size // num_devices
-    # If episode is truncated, there is no value target
-    # So when we compute value loss, we need to mask it
-    value_mask = jnp.cumsum(data.terminated[::-1, :], axis=0)[::-1, :] >= 1
+@hydra.main(version_base=None, config_path="configs", config_name="gardner_chess")
+def main(cfg: DictConfig) -> None:
+    config: Config = Config(**OmegaConf.to_container(cfg, resolve=True))
+    print(config)
 
-    # Compute value target
-    def body_fn(carry, i):
-        ix = config.max_num_steps - i - 1
-        v = data.reward[ix] + data.discount[ix] * carry
-        return v, v
+    env = pgx.make(config.env_id)
+    baseline = pgx.make_baseline_model(config.env_id + "_v0")
 
-    _, value_tgt = jax.lax.scan(
-        body_fn,
-        jnp.zeros(batch_size),
-        jnp.arange(config.max_num_steps),
-    )
-    value_tgt = value_tgt[::-1, :]
-
-    return Sample(
-        obs=data.obs,
-        policy_tgt=data.action_weights,
-        value_tgt=value_tgt,
-        mask=value_mask,
-    )
-
-
-def loss_fn(model_params, model_state, samples: Sample):
-    (logits, value), model_state = forward.apply(
-        model_params, model_state, samples.obs, is_eval=False
-    )
-
-    policy_loss = optax.softmax_cross_entropy(logits, samples.policy_tgt)
-    policy_loss = jnp.mean(policy_loss)
-
-    value_loss = optax.l2_loss(value, samples.value_tgt)
-    value_loss = jnp.mean(value_loss * samples.mask)  # mask if the episode is truncated
-
-    return policy_loss + value_loss, (model_state, policy_loss, value_loss)
-
-
-@partial(jax.pmap, axis_name="i")
-def train(model, opt_state, data: Sample):
-    model_params, model_state = model
-    grads, (model_state, policy_loss, value_loss) = jax.grad(loss_fn, has_aux=True)(
-        model_params, model_state, data
-    )
-    grads = jax.lax.pmean(grads, axis_name="i")
-    updates, opt_state = optimizer.update(grads, opt_state)
-    model_params = optax.apply_updates(model_params, updates)
-    model = (model_params, model_state)
-    return model, opt_state, policy_loss, value_loss
-
-
-@jax.pmap
-def evaluate(rng_key, my_model):
-    """A simplified evaluation by sampling. Only for debugging. 
-    Please use MCTS and run tournaments for serious evaluation."""
-    my_player = 0
-    my_model_params, my_model_state = my_model
-
-    key, subkey = jax.random.split(rng_key)
-    batch_size = config.selfplay_batch_size // num_devices
-    keys = jax.random.split(subkey, batch_size)
-    state = jax.vmap(env.init)(keys)
-
-    def body_fn(val):
-        key, state, R = val
-        (my_logits, _), _ = forward.apply(
-            my_model_params, my_model_state, state.observation, is_eval=True
+    def forward_fn(x, is_eval=False):
+        net = AZNet(
+            num_actions=env.num_actions,
+            num_channels=config.num_channels,
+            num_blocks=config.num_layers,
+            resnet_v2=config.resnet_v2,
         )
-        opp_logits, _ = baseline(state.observation)
-        is_my_turn = (state.current_player == my_player).reshape((-1, 1))
-        logits = jnp.where(is_my_turn, my_logits, opp_logits)
-        key, subkey = jax.random.split(key)
-        action = jax.random.categorical(subkey, logits, axis=-1)
+        policy_out, value_out = net(x, is_training=not is_eval, test_local_stats=False)
+        return policy_out, value_out
+
+    forward = hk.without_apply_rng(hk.transform_with_state(forward_fn))
+    optimizer = optax.adam(learning_rate=config.learning_rate)
+
+    def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
+        # model: params
+        # state: embedding
+        del rng_key
+        model_params, model_state = model
+
+        current_player = state.current_player
         state = jax.vmap(env.step)(state, action)
-        R = R + state.rewards[jnp.arange(batch_size), my_player]
-        return (key, state, R)
 
-    _, _, R = jax.lax.while_loop(
-        lambda x: ~(x[1].terminated.all()), body_fn, (key, state, jnp.zeros(batch_size))
-    )
-    return R
+        (logits, value), _ = forward.apply(model_params, model_state, state.observation, is_eval=True)
+        # mask invalid actions
+        logits = logits - jnp.max(logits, axis=-1, keepdims=True)
+        logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
 
+        reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
+        value = jnp.where(state.terminated, 0.0, value)
+        discount = -1.0 * jnp.ones_like(value)
+        discount = jnp.where(state.terminated, 0.0, discount)
 
-if __name__ == "__main__":
+        recurrent_fn_output = mctx.RecurrentFnOutput(
+            reward=reward,
+            discount=discount,
+            prior_logits=logits,
+            value=value,
+        )
+        return recurrent_fn_output, state
+
+    @jax.pmap
+    def selfplay(model, rng_key: jnp.ndarray) -> SelfplayOutput:
+        model_params, model_state = model
+        batch_size = config.selfplay_batch_size // num_devices
+
+        def step_fn(state, key) -> SelfplayOutput:
+            key1, key2 = jax.random.split(key)
+            observation = state.observation
+
+            (logits, value), _ = forward.apply(
+                model_params, model_state, state.observation, is_eval=True
+            )
+            root = mctx.RootFnOutput(prior_logits=logits, value=value, embedding=state)
+
+            policy_output = mctx.gumbel_muzero_policy(
+                params=model,
+                rng_key=key1,
+                root=root,
+                recurrent_fn=recurrent_fn,
+                num_simulations=config.num_simulations,
+                invalid_actions=~state.legal_action_mask,
+                qtransform=mctx.qtransform_completed_by_mix_value,
+                gumbel_scale=1.0,
+            )
+            actor = state.current_player
+            keys = jax.random.split(key2, batch_size)
+            state = jax.vmap(auto_reset(env.step, env.init))(state, policy_output.action, keys)
+            discount = -1.0 * jnp.ones_like(value)
+            discount = jnp.where(state.terminated, 0.0, discount)
+            return state, SelfplayOutput(
+                obs=observation,
+                action_weights=policy_output.action_weights,
+                reward=state.rewards[jnp.arange(state.rewards.shape[0]), actor],
+                terminated=state.terminated,
+                discount=discount,
+            )
+
+        # Run selfplay for max_num_steps by batch
+        rng_key, sub_key = jax.random.split(rng_key)
+        keys = jax.random.split(sub_key, batch_size)
+        state = jax.vmap(env.init)(keys)
+        key_seq = jax.random.split(rng_key, config.max_num_steps)
+        _, data = jax.lax.scan(step_fn, state, key_seq)
+
+        return data
+
+    @jax.pmap
+    def compute_loss_input(data: SelfplayOutput) -> Sample:
+        batch_size = config.selfplay_batch_size // num_devices
+        # If episode is truncated, there is no value target
+        # So when we compute value loss, we need to mask it
+        value_mask = jnp.cumsum(data.terminated[::-1, :], axis=0)[::-1, :] >= 1
+
+        # Compute value target
+        def body_fn(carry, i):
+            ix = config.max_num_steps - i - 1
+            v = data.reward[ix] + data.discount[ix] * carry
+            return v, v
+
+        _, value_tgt = jax.lax.scan(
+            body_fn,
+            jnp.zeros(batch_size),
+            jnp.arange(config.max_num_steps),
+        )
+        value_tgt = value_tgt[::-1, :]
+
+        return Sample(
+            obs=data.obs,
+            policy_tgt=data.action_weights,
+            value_tgt=value_tgt,
+            mask=value_mask,
+        )
+
+    def loss_fn(model_params, model_state, samples: Sample):
+        (logits, value), model_state = forward.apply(
+            model_params, model_state, samples.obs, is_eval=False
+        )
+
+        policy_loss = optax.softmax_cross_entropy(logits, samples.policy_tgt)
+        policy_loss = jnp.mean(policy_loss)
+
+        value_loss = optax.l2_loss(value, samples.value_tgt)
+        value_loss = jnp.mean(value_loss * samples.mask)  # mask if the episode is truncated
+
+        return policy_loss + value_loss, (model_state, policy_loss, value_loss)
+
+    @partial(jax.pmap, axis_name="i")
+    def train(model, opt_state, data: Sample):
+        model_params, model_state = model
+        grads, (model_state, policy_loss, value_loss) = jax.grad(loss_fn, has_aux=True)(
+            model_params, model_state, data
+        )
+        grads = jax.lax.pmean(grads, axis_name="i")
+        updates, opt_state = optimizer.update(grads, opt_state)
+        model_params = optax.apply_updates(model_params, updates)
+        model = (model_params, model_state)
+        return model, opt_state, policy_loss, value_loss
+
+    @jax.pmap
+    def evaluate(rng_key, my_model):
+        """A simplified evaluation by sampling. Only for debugging.
+        Please use MCTS and run tournaments for serious evaluation."""
+        my_player = 0
+        my_model_params, my_model_state = my_model
+
+        key, subkey = jax.random.split(rng_key)
+        batch_size = config.selfplay_batch_size // num_devices
+        keys = jax.random.split(subkey, batch_size)
+        state = jax.vmap(env.init)(keys)
+
+        def body_fn(val):
+            key, state, R = val
+            (my_logits, _), _ = forward.apply(
+                my_model_params, my_model_state, state.observation, is_eval=True
+            )
+            opp_logits, _ = baseline(state.observation)
+            is_my_turn = (state.current_player == my_player).reshape((-1, 1))
+            logits = jnp.where(is_my_turn, my_logits, opp_logits)
+            key, subkey = jax.random.split(key)
+            action = jax.random.categorical(subkey, logits, axis=-1)
+            state = jax.vmap(env.step)(state, action)
+            R = R + state.rewards[jnp.arange(batch_size), my_player]
+            return (key, state, R)
+
+        _, _, R = jax.lax.while_loop(
+            lambda x: ~(x[1].terminated.all()), body_fn, (key, state, jnp.zeros(batch_size))
+        )
+        return R
 
     # Initialize model and opt_state
     dummy_state = jax.vmap(env.init)(jax.random.split(jax.random.PRNGKey(0), 2))
@@ -361,3 +352,7 @@ if __name__ == "__main__":
                 "frames": frames,
             }
         )
+
+
+if __name__ == "__main__":
+    main()
